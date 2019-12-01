f5cb8626b7c5eaeb93d9fdfadaf6050edee8ed65
Jurģis Krastiņš <jurgis.krastins@fullcontact.com>
2019-12-01 17:24:04 +0200

    add 2018 day 3, 4, 5 & 7 solutions

--- project.clj
+++ project.clj
@@ -2,4 +2,7 @@
   :description "Advent of Code"
   :url "http://adventofcode.com"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/math.combinatorics "0.1.4"]])
+                 [org.clojure/math.combinatorics "0.1.4"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [clj-time "0.15.0"]])
+
